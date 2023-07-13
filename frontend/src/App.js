7185708d0f51c341a7abb5b35828446ff2b5d501
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-07-13 08:54:36 -0300

    Se agrego manejo de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import SidebarWithHeader from './components/SidebarWithHeader';
 import DetalleInvestigador from './pages/DetalleInvestigador';
+import ListaInvestigadores from './pages/ListaInvestigadores';
+
+
+const routes = [
+  // { path: "/", element: <Navigate to="/home" /> },
+  { path: "/investigadores", element: <ListaInvestigadores /> },
+  { path: "/detalle-investigador", element: <DetalleInvestigador /> }
+];
 
 function App() {
   return (
-    <div>
+    <Router>
       <SidebarWithHeader>
-        <DetalleInvestigador />
+        <Routes>
+         {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
       </SidebarWithHeader>
-    </div>
+    </Router>
   );
 }
 
