4e29ced4cd129170bb12f5ecdaa81edaf3028035
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-07-26 13:02:21 -0300

    morgan instalado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import Grupos from './routes/gruposRoutes.js'
+import morgan from 'morgan'
 //importamos nuestro enrutador
 const app = express()
 
 app.use(cors())
+app.use(morgan("dev"))
 app.use(express.json())
 
 app.use('/api/grupos', Grupos)
