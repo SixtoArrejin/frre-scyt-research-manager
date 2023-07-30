1bee71b38d8fb8144e3d5cae1aa3d7555895f07d
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-07-30 00:18:38 -0300

    endpoint get de usaurios solo para pruebas

--- backend/src/repository/usuariosRepository.js
+++ backend/src/repository/usuariosRepository.js
@@ -1,13 +1,8 @@
 import {
     getAll
-} from './baseRepository.js';
-import { prisma } from '../db.js'
-
-export async function getAllUsuarios() {
-    try {
-        const data = await prisma['usuarios'].findMany();
-        return data
-    } catch (error) {
-        throw new Error(`Error al obtener todos los registros de usuarios en la BD: ${error.message}`);
-    }
-}
+  } from './baseRepository.js';
+  import { prisma } from '../db.js'
+  
+  export async function getAllUsuarios() {
+    return await getAll('usuarios');
+  }
