7dd2c007ee1fc5d256ffc21a0dd83ba05ddcffe9
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-08-24 09:41:40 -0300

    endpoint de eliminar categoria

--- frontend/src/utils/api/categoriasApi.js
+++ frontend/src/utils/api/categoriasApi.js
@@ -4,3 +4,7 @@ import { get, post, put, del } from "./baseApi";
 export async function createCategoria(data) {
   return post(`/api/categorias`, data);
 }
+
+export async function deleteCategoria(data) {
+  return del(`/api/categorias/${data.idCategoria}`);
+}
