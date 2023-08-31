9245bebdd82b296aad160a871a27959c439e2942
Tobias Maciel <tobiasmaciel03@gmail.com>
2023-08-31 15:23:47 -0300

    fix: Se arreglan errores de llaves

--- backend/src/controllers/proyectosController.js
+++ backend/src/controllers/proyectosController.js
@@ -3,7 +3,7 @@ import {
   getProyectosPidsService,
   getProyectosExternosService,
   getProyectoByIdService,
-} from '../services/proyectosService.js';
+} from "../services/proyectosService.js";
 
 export async function getProyectos(req, res) {
   try {
@@ -11,12 +11,12 @@ export async function getProyectos(req, res) {
     console.log(tipo, subtipo);
     let proyectos;
 
-    if (tipo === 'pid') {
+    if (tipo === "pid") {
       proyectos = await getProyectosPidsService();
-    } else if (tipo === 'externo') {
-      if (subtipo === 'financiamiento') {
+    } else if (tipo === "externo") {
+      if (subtipo === "financiamiento") {
         proyectos = await getProyectosExternosService(subtipo);
-      } else if (subtipo === 'sinfinanciamiento') {
+      } else if (subtipo === "sinfinanciamiento") {
         proyectos = await getProyectosExternosService(subtipo);
       } else {
         proyectos = await getProyectosExternosService();
@@ -25,7 +25,15 @@ export async function getProyectos(req, res) {
       proyectos = await getAllProyectosService();
     }
 
-    res.status(200).json({ message: `Proyectos encontrados. ${tipo ? `Tipo: ${tipo} ${subtipo ? `- Subtipo: ${subtipo}` : ""} ` : ""}`, success: true, proyectos });
+    res
+      .status(200)
+      .json({
+        message: `Proyectos encontrados. ${
+          tipo ? `Tipo: ${tipo} ${subtipo ? `- Subtipo: ${subtipo}` : ""} ` : ""
+        }`,
+        success: true,
+        proyectos,
+      });
   } catch (error) {
     res.status(500).json({ message: error.message, success: false });
   }
@@ -38,47 +46,65 @@ export async function getProyectosDeGrupo(req, res) {
 
     // Obtener todos los proyectos y luego filtrar por idGrupo
     const proyectos = await getAllProyectosService();
-    const proyectosFiltrados = proyectos.filter(proyecto => proyecto.tiene.some(item => item.idGrupoInvestigacion == idGrupo));
+    const proyectosFiltrados = proyectos.filter((proyecto) =>
+      proyecto.tiene.some((item) => item.idGrupoInvestigacion == idGrupo)
+    );
 
-    res.status(200).json({ message: `Proyectos encontrados para el idGrupoInvestigacion ${idGrupo}.`, success: true, proyectos: proyectosFiltrados });
+    res
+      .status(200)
+      .json({
+        message: `Proyectos encontrados para el idGrupoInvestigacion ${idGrupo}.`,
+        success: true,
+        proyectos: proyectosFiltrados,
+      });
   } catch (error) {
     res.status(500).json({ message: error.message, success: false });
   }
-  
-  export async function getProyectosDePersona(req, res) {
-    try {
-      const { personaId } = req.params;
-      console.log(personaId);
-  
-      // Obtener todos los proyectos y luego filtrar por idGrupo
-      const proyectos = await getAllProyectosService();
-      const proyectosFiltrados = proyectos.filter(proyecto => proyecto.participa.some(item => item.idPersona == personaId));
-      
-      // Mapear los proyectos para mostrar solo el rol correspondiente al ID del proyecto actual
-      const proyectosConRoles = proyectosFiltrados.map(proyecto => {
-        const participacion = proyecto.participa.find(item => item.idPersona == personaId);
-        return {
-          idProyecto: proyecto.idProyecto,
-          tipoActividad: proyecto.tipoActividad,
-          idProyecto: proyecto.idProyecto,
-          tipoActividad: proyecto.tipoActividad,
-          fechaInicio: proyecto.fechaInicio,
-          fechaFin: proyecto.fechaFin,
-          denominacion: proyecto.denominacion,
-          completo: proyecto.completo,
-          regional: proyecto.regional,
-          convocatoria: proyecto.convocatoria,
-          estado: proyecto.estado,
-          rol: participacion.rol,
-        };
+}
+
+export async function getProyectosDePersona(req, res) {
+  try {
+    const { personaId } = req.params;
+    console.log(personaId);
+
+    // Obtener todos los proyectos y luego filtrar por idGrupo
+    const proyectos = await getAllProyectosService();
+    const proyectosFiltrados = proyectos.filter((proyecto) =>
+      proyecto.participa.some((item) => item.idPersona == personaId)
+    );
+
+    // Mapear los proyectos para mostrar solo el rol correspondiente al ID del proyecto actual
+    const proyectosConRoles = proyectosFiltrados.map((proyecto) => {
+      const participacion = proyecto.participa.find(
+        (item) => item.idPersona == personaId
+      );
+      return {
+        idProyecto: proyecto.idProyecto,
+        tipoActividad: proyecto.tipoActividad,
+        idProyecto: proyecto.idProyecto,
+        tipoActividad: proyecto.tipoActividad,
+        fechaInicio: proyecto.fechaInicio,
+        fechaFin: proyecto.fechaFin,
+        denominacion: proyecto.denominacion,
+        completo: proyecto.completo,
+        regional: proyecto.regional,
+        convocatoria: proyecto.convocatoria,
+        estado: proyecto.estado,
+        rol: participacion.rol,
+      };
+    });
+
+    res
+      .status(200)
+      .json({
+        message: `Proyectos encontrados para la personaId ${personaId}.`,
+        success: true,
+        proyectos: proyectosConRoles,
       });
-  
-      res.status(200).json({ message: `Proyectos encontrados para la personaId ${personaId}.`, success: true, proyectos: proyectosConRoles });
-    } catch (error) {
-      res.status(500).json({ message: error.message, success: false });
-    }
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
   }
-  
+}
 
 export async function getProyectoPorId(req, res) {
   try {
@@ -89,9 +115,14 @@ export async function getProyectoPorId(req, res) {
     const proyecto = await getProyectoByIdService(Number(idProyecto));
     // const proyectosFiltrados = proyectos.find(proyecto => proyecto.idProyecto === Number(idProyecto));
 
-    res.status(200).json({ message: `Proyecto encontrado para el proyecto con ID ${idProyecto}.`, success: true, proyecto: proyecto });
+    res
+      .status(200)
+      .json({
+        message: `Proyecto encontrado para el proyecto con ID ${idProyecto}.`,
+        success: true,
+        proyecto: proyecto,
+      });
   } catch (error) {
     res.status(500).json({ message: error.message, success: false });
   }
 }
-
--- frontend/src/utils/api/proyectosApi.js
+++ frontend/src/utils/api/proyectosApi.js
@@ -2,16 +2,21 @@ import { get, post, put, del } from "./baseApi";
 
 // personasServices
 export async function getProyectos(tipo, subtipo) {
-  return get(`/api/proyectos${ tipo ? ("?tipo="+tipo+( subtipo ? ("&subtipo="+subtipo) : "" )) : "" }`)
+  return get(
+    `/api/proyectos${
+      tipo ? "?tipo=" + tipo + (subtipo ? "&subtipo=" + subtipo : "") : ""
+    }`
+  );
 }
 
 export async function getProyectosByIdGrupo(idGrupo) {
-  return get(`/api/proyectos/grupo/${idGrupo}`)
+  return get(`/api/proyectos/grupo/${idGrupo}`);
 }
 
 export async function getProyectosByPersonaId(personaId) {
-  return get(`/api/proyectos/persona/${personaId}`)
-  
+  return get(`/api/proyectos/persona/${personaId}`);
+}
+
 export async function getProyectoById(idProyecto) {
-  return get(`/api/proyectos/${idProyecto}`)
+  return get(`/api/proyectos/${idProyecto}`);
 }
