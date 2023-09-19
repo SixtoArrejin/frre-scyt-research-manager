ca7156141a8990435464e084054d663bd218c98c
Tobias Maciel <tobiasmaciel03@gmail.com>
2023-09-19 12:46:43 -0300

    feat: Se agrega el componente ```CustomModal``` y se lo implementa en Nuevo Grupo

---
+++ frontend/src/components/CustomModal.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+
+export default function CustomModal({
+  isOpen,
+  onClose,
+  guardar = false,
+  title,
+  content,
+  onSave,
+}) {
+  return (
+    <Modal isCentered isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay
+        bg="blackAlpha.300"
+        backdropFilter="blur(10px) hue-rotate(90deg)"
+      />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton onClick={onClose} />
+        <ModalBody>
+          <p>{content}</p>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose}>Cerrar</Button>
+          {guardar && (
+            <Button
+              onClick={() => {
+                onSave();
+                onClose();
+              }}
+              colorScheme="blue"
+            >
+              Guardar
+            </Button>
+          )}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
