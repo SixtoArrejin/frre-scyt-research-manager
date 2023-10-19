d6a51fde8228c7e65fac18bda4e128dad03db041
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-10-19 09:45:16 -0300

    regionales agregadas en bd y back

---
+++ backend/src/controllers/regionalesController.js
@@ -0,0 +1,15 @@
+import {
+  getAllRegionalesService
+} from "../services/regionalesService.js";
+
+export async function getAllRegionales(req, res) {
+  try {
+    var regionales = await getAllRegionalesService();
+
+    regionales = regionales.map(regional => regional.nombre);
+
+    return res.status(200).json({ message: 'Regionales encontradas', success: true, regionales });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
+}
