bba5c1926a4d9a080b15b9e16edfadb1235fb33a
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-11-16 12:23:13 -0300

    detalle en sidebar

--- frontend/src/components/SidebarWithHeader.jsx
+++ frontend/src/components/SidebarWithHeader.jsx
@@ -43,7 +43,7 @@ const LinkItems = [
   { name: 'Investigadores', icon: FiUsers, route: '/investigadores' },
   { name: 'Grupos Investigación', icon: BiNetworkChart, route: '/grupos-investigacion' },
   { name: 'Proyectos', icon: BiTask, route: '/proyectos' },
-  { name: 'Proyectos Externos con Financiamiento', icon: BiTask, route: '/proyectos-externos-financiamiento' },
+  { name: 'Vinculaciones', icon: BiTask, route: '/vinculaciones' },
   // { name: 'Configuración', icon: FiSettings, route: '/investigadores' },
 ];
 
