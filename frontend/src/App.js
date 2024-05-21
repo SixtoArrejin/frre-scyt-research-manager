fdce478cc09d3fcb2eacf0207d7b21f5c6bd7d89
Tobias Maciel <tobiasmaciel03@gmail.com>
2024-05-21 11:44:11 -0300

    fix: ya anda el boton de vinculacion+

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,6 +59,7 @@ const routes = [
   { path: "/proyectos/:idPid/nuevo-desembolso", element: <NuevoDesembolso /> },
   { path: "/proyectos/:idPid/vinculacion/:idVinculacion/nuevo-desembolso", element: <NuevoDesembolso /> },
   { path: "/vinculaciones", element: <ListaVinculaciones /> },
+  { path: "/vinculaciones/nuevo", element: <NuevaVinculacion /> },
   { path: "/vinculaciones/:idVinculacion", element: <DetalleVinculacion /> },
   { path: "*", element: <Navigate to="/home" /> },
 ];
