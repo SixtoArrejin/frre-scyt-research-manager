61b286346a0625f34dee2e50e8619d1c5a21810c
Tobias Maciel <tobiasmaciel03@gmail.com>
2024-06-03 23:42:36 -0300

    feat: Nuevo home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,30 +15,48 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import { Input, HStack, Flex, Spacer, Stack } from "@chakra-ui/react";
-import {
-  Search2Icon,
-  AddIcon,
-  ChevronDownIcon,
-  ChevronRightIcon,
-  ChevronLeftIcon,
-  DeleteIcon,
-  PlusSquareIcon,
-} from "@chakra-ui/icons";
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-  FormControl,
-  FormLabel,
-} from "@chakra-ui/react";
+import { getAllPersonas } from "../utils/api/personasApi";
+import { getAllGrupos } from "../utils/api/gruposApi";
+import { getProyectos } from "../utils/api/proyectosApi";
+import { getVinculaciones } from "../utils/api/vinculacionesApi"; 
+import { useQuery } from "react-query";
 
 export default function Home() {
+  const {
+    data: dataPersonas,
+    isLoading,
+    error,
+  } = useQuery("personas", () => getAllPersonas());
+  const { data: dataGrupos } = useQuery("grupos", () => getAllGrupos());
+  const { data: dataProyectos } = useQuery("proyectos", () => getProyectos());
+  const { data: dataVinculaciones } = useQuery("vinculaciones", () => getVinculaciones());
+
+  // Número total de personas
+  const totalPersonas = dataPersonas?.personas?.length;
+
+  // Número de personas activas
+  const personasActivas = dataPersonas?.personas?.filter(
+    (persona) => persona.activo
+  ).length;
+
+  // Número total de grupos
+  const totalGrupos = dataGrupos?.grupos?.length;
+
+  // Número total de proyectos
+  const totalProyectos = dataProyectos?.proyectos?.length;
+
+  // Número de proyectos no completos
+  const proyectosNoCompletos = dataProyectos?.proyectos?.filter(
+    (proyecto) => !proyecto.completo
+  ).length;
+
+    // Número total de vinculaciones
+    const totalVinculaciones = dataVinculaciones?.vinculaciones?.length;
+
+    // Número de vinculaciones externes
+    const vinculacionesExternas = dataVinculaciones?.vinculaciones?.filter(
+      (vinculacion) => vinculacion.vinculacionessinfinanciamiento === null
+    ).length;
   return (
     <Box margin="10px" display="flex" justifyContent="center" marginTop="3rem">
       <Stack
@@ -52,9 +70,9 @@ export default function Home() {
         <Card key={1} w="300px" minHeight="100px">
           <CardBody>
             <Flex justifyContent="center" alignItems="baseline">
-              <Text fontSize="6xl">69</Text>
+              <Text fontSize="6xl">{personasActivas}</Text>
               <Text fontSize="6xl">/</Text>
-              <Text fontSize="3xl">75</Text>
+              <Text fontSize="3xl">{totalPersonas}</Text>
             </Flex>
           </CardBody>
           <CardFooter
@@ -71,9 +89,9 @@ export default function Home() {
         <Card key={1} w="300px" minHeight="100px">
           <CardBody>
             <Flex justifyContent="center" alignItems="baseline">
-              <Text fontSize="6xl">22</Text>
+              <Text fontSize="6xl">{proyectosNoCompletos}</Text>
               <Text fontSize="6xl">/</Text>
-              <Text fontSize="3xl">24</Text>
+              <Text fontSize="3xl">{totalProyectos}</Text>
             </Flex>
           </CardBody>
           <CardFooter
@@ -83,16 +101,16 @@ export default function Home() {
             justifyContent="center"
           >
             <Heading size="xs" textTransform="uppercase" textAlign="center">
-               Proyectos Activos
+              Proyectos Activos
             </Heading>
           </CardFooter>
         </Card>
         <Card key={1} w="300px" minHeight="100px">
           <CardBody>
             <Flex justifyContent="center" alignItems="baseline">
-              <Text fontSize="6xl">15</Text>
+              <Text fontSize="6xl">{vinculacionesExternas}</Text>
               <Text fontSize="6xl">/</Text>
-              <Text fontSize="3xl">24</Text>
+              <Text fontSize="3xl">{totalVinculaciones}</Text>
             </Flex>
           </CardBody>
           <CardFooter
@@ -109,7 +127,7 @@ export default function Home() {
         <Card key={1} w="300px" minHeight="100px">
           <CardBody>
             <Flex justifyContent="center" alignItems="baseline">
-              <Text fontSize="6xl">6</Text>
+              <Text fontSize="6xl">{totalGrupos}</Text>
             </Flex>
           </CardBody>
           <CardFooter
