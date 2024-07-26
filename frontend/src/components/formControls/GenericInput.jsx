991b5a6408ea1838e4b9d150f1af58bd9861678e
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2024-07-26 20:08:31 -0300

    fix

--- frontend/src/components/formControls/GenericInput.jsx
+++ frontend/src/components/formControls/GenericInput.jsx
@@ -1,16 +1,29 @@
-import React from "react";
-import { FormControl, FormLabel, Input, Text } from "@chakra-ui/react";
+import React from 'react';
+import { FormControl, FormLabel, Input, Text } from '@chakra-ui/react';
 
-const GenericInput = ({ id, name, label, placeholder, register, errors, type = "text", width }) => (
-  <FormControl variant="floating" id={id} mb="5vh" isRequired width={width}>
+const GenericInput = ({
+  id,
+  name,
+  label,
+  placeholder,
+  register,
+  errors,
+  type = 'text',
+  width,
+  value,
+  isDisabled = false,
+}) => (
+  <FormControl variant='floating' id={id} mb='5vh' isRequired width={width}>
     <Input
       name={name}
       placeholder={placeholder}
       type={type}
       {...register(name)}
+      value={value}
+      isDisabled={isDisabled}
     />
     <FormLabel>{label}</FormLabel>
-    <Text fontSize="sm" color="red">
+    <Text fontSize='sm' color='red'>
       {errors[name]?.message}
     </Text>
   </FormControl>
