877ffe32b0e7455a51c8359fd1e182d71d235523
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2024-07-29 22:27:17 -0300

    inputs actualizados en lista de proyectos

--- frontend/src/pages/proyectos/Proyectos.jsx
+++ frontend/src/pages/proyectos/Proyectos.jsx
@@ -1,113 +1,69 @@
-import React, { useState, useEffect } from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Text,
-  Heading,
-  Box,
-  Button,
-  Checkbox,
-  IconButton,
-} from "@chakra-ui/react";
-import { Input, HStack } from "@chakra-ui/react";
-import {
-  Search2Icon,
-  AddIcon,
-  ChevronDownIcon,
-  ChevronRightIcon,
-  ChevronLeftIcon,
-  PlusSquareIcon,
-} from "@chakra-ui/icons";
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from "@chakra-ui/react";
-// import investigadores from "../utils/data/investigadores.json";
-import InputLabel from "../../components/InputLabel";
-import { Link, useHistory, useLocation, useNavigate } from "react-router-dom";
-import { getAllGrupos } from "../../utils/api/gruposApi";
-import { useQuery } from "react-query";
-import { getProyectos } from "../../utils/api/proyectosApi";
-import Tabla from "../../components/Tabla";
-import { formatoFechaISOaDDMMAAAA } from "../../utils/general";
+import React, { useState, useEffect } from 'react';
+import { Card, CardBody, Text, Heading, Box, Button } from '@chakra-ui/react';
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getProyectos } from '../../utils/api/proyectosApi';
+import Tabla from '../../components/Tabla';
+import { formatoFechaISOaDDMMAAAA } from '../../utils/general';
+import GenericInput from '../../components/formControls/GenericInput';
 
 export default function ProyectosPid() {
-  const [codPID, setCodPID] = useState("");
-  const [denominacion, setDenominacion] = useState("");
+  const [codPID, setCodPID] = useState('');
+  const [denominacion, setDenominacion] = useState('');
   const [filtro, setFiltro] = useState(false);
 
-  const { data, isLoading, error } = useQuery("proyectos", () => getProyectos());
+  const { data, isLoading, error } = useQuery('proyectos', () => getProyectos());
   const [proyectos, setProyectos] = useState(data?.proyectos || []);
-  console.log(proyectos)
-
+  
   useEffect(() => {
-    if (codPID === "" && denominacion === "") {
+    if (codPID === '' && denominacion === '') {
       // Si no se está filtrando nada, utiliza los datos originales data?.personas
       setProyectos(data?.proyectos || []);
       setFiltro(false);
     } else {
       const filteredProyectos = data?.proyectos.filter(
-        (item) =>
-          (item.codPid.toLowerCase().includes(codPID.toLowerCase())) &&
-          item.denominacion.toLowerCase().includes(denominacion.toLowerCase())
+        (item) => item.codPid.toLowerCase().includes(codPID.toLowerCase()) && item.denominacion.toLowerCase().includes(denominacion.toLowerCase())
       );
       setProyectos(filteredProyectos);
-      setFiltro(true)
+      setFiltro(true);
     }
   }, [codPID, denominacion, data]);
 
   if (isLoading) {
-    return <Text fontSize="md">Cargando...</Text>;
+    return <Text fontSize='md'>Cargando...</Text>;
   }
 
   return (
     <Card>
       <CardBody>
-        <Box
-          display="flex"
-          flexDirection="column"
-          width="100%"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Heading as="h2" size="xl" textAlign="center">
+        <Box display='flex' flexDirection='column' width='100%' alignItems='center' justifyContent='center'>
+          <Heading as='h2' size='xl' textAlign='center'>
             Proyectos
           </Heading>
 
           <br />
 
-          <Box display="flex" width="100%">
-            <Box
-              display="flex"
-              justifyContent="space-between"
-              width="45%"
-              marginLeft="2%"
-            >
-              <InputLabel
-                placeholder="Código PID"
-                width="15vw"
-                onChange={(event) => setCodPID(event.target.value)}
+          <Box display='flex' width='100%'>
+            <Box display='flex' justifyContent='space-between' width='45%' marginLeft='2%'>
+              <GenericInput
+                placeholder='Código PID'
+                label='Código PID'
+                width='15vw'
                 value={codPID}
+                onChange={(event) => setCodPID(event.target.value)}
               />
-              <InputLabel
-                placeholder="Denominación"
-                width="15vw"
-                onChange={(event) => setDenominacion(event.target.value)}
+              <GenericInput
+                placeholder='Denominación'
+                label='Denominación'
+                width='15vw'
                 value={denominacion}
+                onChange={(event) => setDenominacion(event.target.value)}
               />
             </Box>
-            <Box display="flex" justifyContent="flex-end" width="55%">
-              <Link to={"nuevo"}>
-                <Button colorScheme="blue" variant="outline" mr="5">
+            <Box display='flex' justifyContent='flex-end' width='55%'>
+              <Link to={'nuevo'}>
+                <Button colorScheme='blue' variant='outline' mr='5'>
                   Proyecto PID +
                 </Button>
               </Link>
@@ -118,10 +74,11 @@ export default function ProyectosPid() {
           <Tabla
             columnas={['Cod. PID', 'Fecha Inicio', 'Denominación', 'Regional', 'Estado', 'Ver Más']}
             datos={proyectos?.map((item, index) => {
-              const denominacion = item?.denominacion === item?.denominacion.substring(0, 40) ? item?.denominacion : item?.denominacion.substring(0, 40) + '...'
+              const denominacion =
+                item?.denominacion === item?.denominacion.substring(0, 40) ? item?.denominacion : item?.denominacion.substring(0, 40) + '...';
               var regional = item?.regional;
-              if (item?.regional.startsWith("Facultad Regional")) {
-                var regional = "F.R." + regional.substring("Facultad Regional".length);
+              if (item?.regional.startsWith('Facultad Regional')) {
+                var regional = 'F.R.' + regional.substring('Facultad Regional'.length);
               }
 
               return [
@@ -130,14 +87,13 @@ export default function ProyectosPid() {
                 denominacion,
                 regional,
                 item?.estado.charAt(0).toUpperCase() + item?.estado.toLowerCase().substring(1),
-                <Link to={`/proyectos/${item.idProyecto}`} >
+                <Link to={`/proyectos/${item.idProyecto}`}>
                   <PlusSquareIcon />
-                </Link>
-              ]
+                </Link>,
+              ];
             })}
             filtro={filtro}
           />
-
         </Box>
       </CardBody>
     </Card>
