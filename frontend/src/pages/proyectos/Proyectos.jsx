807b1913f70b5ee96bc5582b9cca1f0c793a5cd0
TobiasMaciel <tobiasmaciel03@gmail.com>
2025-03-11 06:51:37 -0300

    feat: Se filtra los proyectos por fecha de mas nuevo a mas viejos

--- frontend/src/pages/proyectos/Proyectos.jsx
+++ frontend/src/pages/proyectos/Proyectos.jsx
@@ -1,106 +1,149 @@
-import React, { useState, useEffect } from 'react';
-import { Card, CardBody, Text, Heading, Box, Button, Spinner, Tooltip } from '@chakra-ui/react';
-import { PlusSquareIcon } from '@chakra-ui/icons';
-import { CheckIcon, CloseIcon } from '@chakra-ui/icons';
-import { Link } from 'react-router-dom';
-import { useQuery } from 'react-query';
-import { getProyectos } from '../../utils/api/proyectosApi';
-import Tabla from '../../components/Tabla';
-import { formatoFechaISOaDDMMAAAA } from '../../utils/general';
-import GenericInput from '../../components/formControls/GenericInput';
-import GenericSelect from '../../components/formControls/GenericSelect';
-import ImgDefault from '../../components/ImgDefault';
-import NoData from '../../img/no-data-2.png';
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardBody,
+  Text,
+  Heading,
+  Box,
+  Button,
+  Spinner,
+  Tooltip,
+} from "@chakra-ui/react";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { getProyectos } from "../../utils/api/proyectosApi";
+import Tabla from "../../components/Tabla";
+import { formatoFechaISOaDDMMAAAA } from "../../utils/general";
+import GenericInput from "../../components/formControls/GenericInput";
+import GenericSelect from "../../components/formControls/GenericSelect";
+import ImgDefault from "../../components/ImgDefault";
+import NoData from "../../img/no-data-2.png";
 
 export default function ProyectosPid() {
-  const [codPID, setCodPID] = useState('');
-  const [denominacion, setDenominacion] = useState('');
+  const [codPID, setCodPID] = useState("");
+  const [denominacion, setDenominacion] = useState("");
   const [filtro, setFiltro] = useState(false);
+  const [pidExterno, setPidExterno] = useState("todos");
 
-  const { data, isLoading, error } = useQuery('proyectos', () => getProyectos());
-  const [proyectos, setProyectos] = useState(data?.proyectos || []);
-  const [pidExterno, setPidExterno] = useState('todos');
+  const { data, isLoading, error } = useQuery("proyectos", () =>
+    getProyectos()
+  );
+  const [proyectos, setProyectos] = useState([]);
 
   useEffect(() => {
-    if (codPID === '' && denominacion === '' && pidExterno === 'todos') {
-      // Mostrar todos los proyectos sin filtrar
-      setProyectos(data?.proyectos || []);
-      setFiltro(false);
-    } else {
-      let filteredProyectos;
+    if (data?.proyectos) {
+      let filteredProyectos = [...data.proyectos];
 
-      // Si hay un valor en codPID, primero filtramos solo aquellos proyectos que tienen codPid
-      if (codPID) {
-        const proyectosConCodPid = data?.proyectos.filter((item) => item.codPid && item.codPid.toLowerCase().includes(codPID.toLowerCase()));
-
-        // Luego filtramos por denominacion en los que tienen codPid
-        filteredProyectos = proyectosConCodPid.filter((item) => item.denominacion.toLowerCase().includes(denominacion.toLowerCase()));
-        setPidExterno('pid');
+      // Aplicar filtros
+      if (codPID || denominacion || pidExterno !== "todos") {
+        filteredProyectos = filteredProyectos.filter((item) => {
+          const matchesCodPID = codPID
+            ? item.codPid &&
+              item.codPid.toLowerCase().includes(codPID.toLowerCase())
+            : true;
+          const matchesDenominacion = denominacion
+            ? item.denominacion
+                .toLowerCase()
+                .includes(denominacion.toLowerCase())
+            : true;
+          const matchesPIDExterno =
+            pidExterno === "pid"
+              ? item.codPid
+              : pidExterno === "externos"
+              ? !item.codPid
+              : true;
+          return matchesCodPID && matchesDenominacion && matchesPIDExterno;
+        });
+        setFiltro(true);
       } else {
-        let typeFilter;
-        if (pidExterno === 'pid') {
-          typeFilter = data?.proyectos.filter((item) => item.codPid && item.codPid.toLowerCase().includes(codPID.toLowerCase()));
-        } else if (pidExterno === 'externos') {
-          typeFilter = data?.proyectos.filter((item) => !item.codPid);
-        } else {
-          typeFilter = data?.proyectos || [];
-        }
-        filteredProyectos = typeFilter.filter((item) => item.denominacion.toLowerCase().includes(denominacion.toLowerCase()));
+        setFiltro(false);
       }
 
-      setProyectos(filteredProyectos);
-      setFiltro(true);
+      // Ordenar por fechaInicio (de más nuevo a más viejo)
+      const proyectosOrdenados = filteredProyectos.sort((a, b) => {
+        const fechaA = a.fechaInicio ? new Date(a.fechaInicio) : new Date(0);
+        const fechaB = b.fechaInicio ? new Date(b.fechaInicio) : new Date(0);
+        return fechaB - fechaA;
+      });
+
+      setProyectos(proyectosOrdenados);
     }
-  }, [codPID, denominacion, pidExterno, data]);
+  }, [data, codPID, denominacion, pidExterno]);
 
   if (isLoading) {
     return (
-      <Box display='flex' height='calc(100vh - 80px - 16px - 1px - 16px)' width='100%' alignItems='center' justifyContent='center'>
-        <Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl' />
+      <Box
+        display="flex"
+        height="calc(100vh - 80px - 16px - 1px - 16px)"
+        width="100%"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
       </Box>
     );
   }
+
   return (
     <Card>
       <CardBody>
-        <Box display='flex' flexDirection='column' width='100%' alignItems='center' justifyContent='center'>
-          <Heading as='h2' size='xl' textAlign='center'>
+        <Box
+          display="flex"
+          flexDirection="column"
+          width="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Heading as="h2" size="xl" textAlign="center">
             Proyectos
           </Heading>
 
           <br />
 
-          <Box display='flex' width='100%'>
-            <Box display='flex' justifyContent='space-between' width='85%' marginLeft='2%'>
+          <Box display="flex" width="100%">
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              width="85%"
+              marginLeft="2%"
+            >
               <GenericInput
-                placeholder='Código PID'
-                label='Código PID'
-                width='15vw'
+                placeholder="Código PID"
+                label="Código PID"
+                width="15vw"
                 value={codPID}
                 onChange={(event) => setCodPID(event.target.value)}
               />
               <GenericInput
-                placeholder='Denominación'
-                label='Denominación'
-                width='15vw'
+                placeholder="Denominación"
+                label="Denominación"
+                width="15vw"
                 value={denominacion}
                 onChange={(event) => setDenominacion(event.target.value)}
               />
               <GenericSelect
-                name='pidExterno'
-                width='15vw'
+                name="pidExterno"
+                width="15vw"
                 options={[
-                  { value: 'todos', label: 'Todos' },
-                  { value: 'pid', label: 'PID' },
-                  { value: 'externos', label: 'Externos' },
+                  { value: "todos", label: "Todos" },
+                  { value: "pid", label: "PID" },
+                  { value: "externos", label: "Externos" },
                 ]}
                 value={pidExterno}
                 onChange={(event) => setPidExterno(event.target.value)}
               />
             </Box>
-            <Box display='flex' justifyContent='flex-end' width='55%'>
-              <Link to={'nuevo'}>
-                <Button colorScheme='blue' variant='outline' mr='5'>
+            <Box display="flex" justifyContent="flex-end" width="55%">
+              <Link to={"nuevo"}>
+                <Button colorScheme="blue" variant="outline" mr="5">
                   Proyecto +
                 </Button>
               </Link>
@@ -108,35 +151,68 @@ export default function ProyectosPid() {
           </Box>
 
           <br />
-          {proyectos?.length > 0 ? (<Tabla
-            columnas={['Externo','Cod. PID', 'Fecha Inicio', 'Denominación', 'Estado', 'Ver Más']}
-            datos={proyectos?.map((item) => {
-              const denominacion =
-                item?.denominacion === item?.denominacion.substring(0, 40) ? item?.denominacion : item?.denominacion.substring(0, 40) + '...';
+          {proyectos?.length > 0 ? (
+            <Tabla
+              columnas={[
+                "Externo",
+                "Cod. PID",
+                "Fecha Inicio",
+                "Denominación",
+                "Estado",
+                "Ver Más",
+              ]}
+              datos={proyectos?.map((item) => {
+                const denominacion =
+                  item?.denominacion === item?.denominacion.substring(0, 40)
+                    ? item?.denominacion
+                    : item?.denominacion.substring(0, 40) + "...";
                 const externo = item?.codPid ? (
-                  <Tooltip openDelay={100} hasArrow label="Proyecto externo" bg="red.500" color="white">
+                  <Tooltip
+                    openDelay={100}
+                    hasArrow
+                    label="Proyecto externo"
+                    bg="red.500"
+                    color="white"
+                  >
                     <CloseIcon color="red.500" />
                   </Tooltip>
                 ) : (
-                  <Tooltip openDelay={100} hasArrow label="Proyecto PID" bg="green.500" color="white">
+                  <Tooltip
+                    openDelay={100}
+                    hasArrow
+                    label="Proyecto PID"
+                    bg="green.500"
+                    color="white"
+                  >
                     <CheckIcon color="green.500" />
                   </Tooltip>
                 );
-                const fechaInicio = item?.fechaInicio ? formatoFechaISOaDDMMAAAA(item.fechaInicio) : "-";
-              return [
-                externo,
-                item.codPid ? item.codPid : '-',
-                fechaInicio,
-                denominacion,
-                item?.estado ? item?.estado.charAt(0).toUpperCase() + item?.estado.toLowerCase().substring(1) : '-',
-                <Link to={`/proyectos/${item.idProyecto}`}>
-                  <PlusSquareIcon />
-                </Link>,
-              ];
-            })}
-            filtro={filtro}
-          />) : (
-            <ImgDefault src={NoData} alt='No Data' width='30%' text='No hay proyectos para mostrar.' />
+                const fechaInicio = item?.fechaInicio
+                  ? formatoFechaISOaDDMMAAAA(item.fechaInicio)
+                  : "-";
+                return [
+                  externo,
+                  item.codPid ? item.codPid : "-",
+                  fechaInicio,
+                  denominacion,
+                  item?.estado
+                    ? item?.estado.charAt(0).toUpperCase() +
+                      item?.estado.toLowerCase().substring(1)
+                    : "-",
+                  <Link to={`/proyectos/${item.idProyecto}`}>
+                    <PlusSquareIcon />
+                  </Link>,
+                ];
+              })}
+              filtro={filtro}
+            />
+          ) : (
+            <ImgDefault
+              src={NoData}
+              alt="No Data"
+              width="30%"
+              text="No hay proyectos para mostrar."
+            />
           )}
         </Box>
       </CardBody>
